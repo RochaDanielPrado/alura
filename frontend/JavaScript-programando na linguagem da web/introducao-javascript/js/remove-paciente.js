5061c327812b7088ad10bd019ca9ab55006e531c
RochaDanielPrado <daniel.rocha@blueone.eco.br>
2023-01-20 09:39:46 -0300

    js

--- frontend/JavaScript-programando na linguagem da web/introducao-javascript/js/remove-paciente.js
+++ frontend/JavaScript-programando na linguagem da web/introducao-javascript/js/remove-paciente.js
@@ -1,3 +1,8 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-console.log(pacientes);
+pacientes.forEach(function(paciente){
+    paciente.addEventListener("dblclick", function(){
+        console.log("fui clidado com duplo click");
+        this.remove();
+    });
+})
