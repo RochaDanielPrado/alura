5d97dae314e2514de3cd6ae52f4a104da74032b7
RochaDanielPrado <daniel.rocha@blueone.eco.br>
2023-04-07 18:16:10 -0300

    fim 13

---
+++ frontend/13-alura_books/app/main.js
@@ -0,0 +1,17 @@
+//import { exibirOsLivrosNaTela } from "./metodoForEach.js";
+//import { aplicarDesconto } from "./metodoMap.js";
+//import { filtraLivrosDeFront } from "./metodoFilter.js";
+
+let livros = [];
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+
+getBuscarLivrosDaAPI();
+
+async function getBuscarLivrosDaAPI() {
+    const res = await fetch(endpointDaAPI);
+    livros = await res.json();
+    let livrosComDesconto = aplicarDesconto(livros);
+    exibirOsLivrosNaTela(livrosComDesconto);
+}
+
+
