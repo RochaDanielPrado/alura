d64c54535223c001753c1a94eaa5e1669bac74b9
RochaDanielPrado <daniel.rocha@blueone.eco.br>
2023-04-17 11:05:04 -0300

    react

--- frontend/21 - organo/organo/src/componentes/Formulario/index.js
+++ frontend/21 - organo/organo/src/componentes/Formulario/index.js
@@ -24,9 +24,9 @@ const Formulario = () => {
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-                <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto obrigatorio={false} label="Imagem" placeholder="Informe o endereço da imagem" />
                 <ListaSuspensa label="Time" itens={times} />
                 <Botao>Criar Card</Botao>
             </form>
