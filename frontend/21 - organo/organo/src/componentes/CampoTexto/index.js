8c0461102cddb81ac6868e12c8fb9c44d83067f7
RochaDanielPrado <daniel.rocha@blueone.eco.br>
2023-04-17 17:41:31 -0300

    react

--- frontend/21 - organo/organo/src/componentes/CampoTexto/index.js
+++ frontend/21 - organo/organo/src/componentes/CampoTexto/index.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import './CampoTexto.css';
 
 const CampoTexto = (props) => {
+
+    let valor = '';
+
+    useState('');
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value;
+        console.log(valor);
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={props.placeholder} required={props.obrigatorio}/>
+            <input value={valor} onChange={aoDigitado} placeholder={props.placeholder} required={props.obrigatorio}/>
         </div>
     );
 };
