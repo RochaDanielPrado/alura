ce6481cdf3bee7c417d91ffce3a05d2d270d1765
RochaDanielPrado <daniel.rocha@rocce.com.br>
2023-04-19 13:43:36 -0300

    seg

--- frontend/21 - organo/organo/src/componentes/Colaborador/index.js
+++ frontend/21 - organo/organo/src/componentes/Colaborador/index.js
@@ -1,10 +1,11 @@
 import './Colaborador.css';
+{/*https://github.com/RochaDanielPrado.png*/}
 
 const Colaborador = ({nome, cargo, imagem}) => {
     return (
         <div className='colaborador'>
             <div className='cabecalho'>
-                <img src={imagem} alt='Daniel Rocha' />
+                <img src={imagem} alt={nome} />
             </div>
             <div className='rodape'>
                 <h4>{nome}</h4>
